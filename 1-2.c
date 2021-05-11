62c27550b0435e26beb38601befa80ab249e920a
wyluilipe <65451026+wyluilipe@users.noreply.github.com>
2021-05-11 18:32:17 +0300

    Add files via upload

---
+++ 1-2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int aflag = 0;
+int iter = 0;
+
+void alrm(int signo)
+{
+	signal(SIGALRM, alrm);
+	iter = 0;
+}
+
+void handler(int signo)
+{
+	signal(SIGINT, handler);
+	iter++;
+	if (iter == 1)
+		alarm(1);
+	if (iter == 3)
+		_exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	int c;
+	signal(SIGALRM, alrm);
+	signal(SIGINT, handler);
+	while ((c = getchar()) != EOF)
+		if (c == '\n')
+			printf("OK\n");
+	printf("GoodBye!\n");
+	return 0;
+}
